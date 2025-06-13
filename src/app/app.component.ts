5fe8dcb5c5b99069f8484014cd9e4b4c560ac7f5
GaetanThomas42 <gaetanthomasdev@gmail.com>
2025-06-13 16:44:46 +0200

    Navbar, Home, Footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
