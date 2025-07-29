49bdf5bfd02889ddd5a143cb8b8b3f2d09a2dec5
gaetanthomasdev <gaetanthomasdev@gmail.com>
2025-07-29 11:54:07 +0200

    Navbar active links

--- src/app/components/parts/nav-bar/nav-bar.component.ts
+++ src/app/components/parts/nav-bar/nav-bar.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink,CommonModule,RouterLinkActive],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 
